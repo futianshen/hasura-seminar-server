f90884e22c8f02d6869f243ecd148144709aada9
沈輔天 <farrant@macbook.local>
2021-05-01 22:07:21 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import cors from "cors"
+import dotenv from "dotenv"
+import dotenvFlow from "dotenv-flow"
+import express from "express"
+import router from "./router"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+dotenv.config()
+dotenvFlow.config()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
