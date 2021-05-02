bb05924235d12cca66b85c474f979a02a3f5aff4
沈輔天 <farrant@macbook.local>
2021-05-02 17:29:32 +0800

    feat: add router

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import cors from "cors"
 import dotenv from "dotenv"
 import dotenvFlow from "dotenv-flow"
 import express from "express"
+import router from "./router"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(router)
 
 dotenv.config()
 dotenvFlow.config()
